8154dce19f9f26e9734f13bb0fd0d0d952bf3650
Damien Di Fede <dcomposer@gmail.com>
2017-09-09 17:15:47 -0500

    Initial Commit

---
+++ Blinds/BlindsView.h
@@ -0,0 +1,13 @@
+//
+//  BlindsView.h
+//  Blinds
+//
+//  Created by Damien Di Fede on 9/9/17.
+//  Copyright © 2017 compartmental. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BlindsView : ScreenSaverView
+
+@end
---
+++ Blinds/BlindsView.m
@@ -0,0 +1,52 @@
+//
+//  BlindsView.m
+//  Blinds
+//
+//  Created by Damien Di Fede on 9/9/17.
+//  Copyright © 2017 compartmental. All rights reserved.
+//
+
+#import "BlindsView.h"
+
+@implementation BlindsView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
